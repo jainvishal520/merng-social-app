63d611451247edd2e04b6adae58f1ac7f0958631
jainvishal520 <jainvishal520@gmail.com>
2020-12-06 20:36:24 +0530

    Apollo setup - apollo provider

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,24 @@
+// will export a jsx element
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+// creating link to graphql endpoint
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000",
+});
+
+// creating apollo client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
