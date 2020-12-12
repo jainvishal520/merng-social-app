9a8e4765deea2d18c8b33fe143c8e0742a11a040
jainvishal520 <jainvishal520@gmail.com>
2020-12-12 21:41:01 +0530

    creating post mutation graphql

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
+import { AuthContext } from "../context/AuthContext";
+import PostForm from "../components/PostForm";
+
 // import PostCard from "../components/PostCard";
 const Home = () => {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-
+  const { user } = useContext(AuthContext);
   return (
     <Grid columns={3}>
       <Grid.Row>
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>loading posts ...</h1>
         ) : (
