54422fb968b0ac5f0976d1cfe1b32b77d21c39d5
jainvishal520 <jainvishal520@gmail.com>
2020-12-12 21:40:30 +0530

    apollo link context - interceptor for server req

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -5,15 +5,30 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-
+import { setContext } from "apollo-link-context";
 // creating link to graphql endpoint
 const httpLink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
+// This is kind of interceptor .. before sending it to server it will intercept and can do stuff
+// here .. like adding authorization header with each req
+const authLink = setContext(() => {
+  const user = localStorage.getItem("user");
+  let token = "";
+  if (user) {
+    token = JSON.parse(user).token;
+  }
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 // creating apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
